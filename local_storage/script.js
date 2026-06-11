1eafe14330ad8c74fabc4fe38f13ed8196c5c07d
Tridib Dey <porasuna2009@gmail.com>
2026-06-11 16:47:44 +0530

    learnt using local strage as well as session storage

---
+++ local_storage/script.js
@@ -0,0 +1,19 @@
+// // setting val in local storage:
+
+// localStorage.setItem("name" , "Tridib");
+// localStorage.setItem("name","Arunima")
+// localStorage.setItem("theme" , "dark");
+// console.log(localStorage.getItem("name"));
+// localStorage.removeItem("name");
+// localStorage.clear()
+
+// localStorage.setItem("name","Tridib");
+// localStorage.setItem("Frnds", JSON.stringify(["Arunima","Kundu","prithvi"])) // use JSON.parse for retrieval as i  inserted
+
+
+// document.cookie = "name=Tridib"
+
+
+//  sessionStorage :  Same as localstorage
+
+
